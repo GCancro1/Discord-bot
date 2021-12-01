afaa9d0ecb5897b4e5b23f05400bb848005b29df
george <george.cancro@yahoo.com>
2021-12-01 10:45:06 -0500

    write multiple txt files to discord from config

--- repeat-bot.py
+++ repeat-bot.py
@@ -8,30 +8,43 @@ with open("discord_config.yml", "r") as ymlfile:
 
 print(configs)
 
-token = configs["token"]
-my_guild = configs["guild"]
+
+TOKEN = configs["token"]
+GUILD = configs["guild"]
+CHANNEL_ID = configs["channel"]
+MESSAGE_FILES = configs["message_files"]
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
 
-
 @client.event
 async def on_ready():
     for guild in client.guilds:
-        if guild.name == my_guild:
+        if guild.name == GUILD:
             break
 
     print(
         f"{client.user} is connected to the following guild:\n"
         f"{guild.name}(id: {guild.id})"
+        f"{guild.channels}"
     )
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    await message.channel.send(message)
+    channel = client.get_channel(CHANNEL_ID)
+    for file in MESSAGE_FILES:
+        with open(file, "r") as f:
+            message = f.read()
+            print(f"Sending message: \n{message} to channel: {channel}\n")
+            await channel.send(message)
+    print("logging out")
+    await client.close()
 
-client.run(token)
+# basic repeat message
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+#     await message.channel.send(message)
+
+client.run(TOKEN)
 
